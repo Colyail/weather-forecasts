5d3c9216cca2f4dc7d670ebae936d1daa167aac6
Dennis <destroyman0211@gmail.com>
2024-03-13 07:07:49 -0400

    feat: implement search feature

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,9 +6,13 @@ import SettingsModal from 'components/SettingsModal';
 import TextSection from 'components/TextSection';
 import { setTheme, ThemeTypes } from '../../redux/themeSlice';
 import { TimeFormat } from '../../redux/settingsSlice';
+import { setSelectedCity } from '../../redux/citySlice';
 import { getFullTimeFormat, getMeridiemTimeFormat } from 'utils/getTimeFromData';
 import { ReactComponent as ThemeSwitchBlackIcon } from 'assets/theme-switch-black.svg'
 import { ReactComponent as ThemeSwitchWhiteIcon } from 'assets/theme-switch-white.svg'
+import { ReactComponent as CloseCircleBlackIcon } from 'assets/close-circle-black.svg';
+import { ReactComponent as CloseCircleWhiteIcon } from 'assets/close-circle-white.svg';
+import cities from 'utils/Cities.json'
 
 const Header = () => {
   const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
@@ -18,6 +22,8 @@ const Header = () => {
 
   const [time, setTime] = useState<Date>(new Date());
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const [isOpenSearch, setIsOpenSearch] = useState<boolean>(false);
+  const [searchInput, setSearchInput] = useState<string>("");
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -33,6 +39,20 @@ const Header = () => {
     setIsOpenModal(true);
   }
 
+  const handleInputEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      const matchedCity = cities.find(city => city.toLowerCase() === searchInput.toLowerCase());
+      if (matchedCity) {
+        dispatch(setSelectedCity(matchedCity))
+      }
+    }
+  }
+
+  const handleClickClose = () => {
+    setIsOpenSearch(false);
+    setSearchInput("");
+  }
+
   return (
     <HeaderContainer>
       <TextSection>
@@ -43,9 +63,26 @@ const Header = () => {
         }
       </TextSection>
       <ToolsWrapper>
-        <ToolButton>
-          Search
-        </ToolButton>
+        {
+          isOpenSearch ?
+          <SearchBarContainer>
+            <SearchTerm
+              type="text"
+              placeholder="Search"
+              value={searchInput}
+              onChange={(event) => setSearchInput(event.target.value)}
+              onKeyDown={(event) => handleInputEnter(event)}
+            />
+            {
+              theme === ThemeTypes.Dark ?
+              <CloseCircleBlackIcon className='close' onClick={handleClickClose}/> :
+              <CloseCircleWhiteIcon className='close' onClick={handleClickClose}/>
+            }
+          </SearchBarContainer> :
+          <ToolButton onClick={() => setIsOpenSearch(true)}>
+            Search
+          </ToolButton>
+        }
         <ToolButton onClick={handleClickSettings}>
           Settings
         </ToolButton>
@@ -73,6 +110,33 @@ const ToolsWrapper = styled.div`
   display: flex;
   gap: 16px;
   align-items: center;
+`
+
+const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  .close {
+    right: 8px;
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+  }
+`
+
+const SearchTerm = styled.input`
+  width: 100%;
+  border: ${props => `2px solid ${props.theme.primary}`};
+  padding: 5px;
+  padding-right: 28px;
+  height: 20px;
+  border-radius: 5px;
+  outline: none;
+  background: ${props => props.theme.background};
+  color: ${props => props.theme.text};
+  font-size: 20px;
 `
 
 const ToolButton = styled.div`
